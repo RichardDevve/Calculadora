2f836a40bf17c42e551adae0af4a1ece2f56a1bb
RichardDevve <157928092+RichardDevve@users.noreply.github.com>
2024-01-28 10:12:04 -0500

    Add files via upload
    Esta calculadora esta mejor hecha

--- Calculadora.py
+++ Calculadora.py
@@ -1,19 +1,50 @@
-n1 = input("Ingresa el primer numero: ")
-n2 = input("Ingresa el segundo numero: ")
+# Definir funciones para operaciones matematicas
 
-n1 = int(n1)
-n2 = int(n2)
+def suma(x, y) :
+    return x + y
 
-suma = n1 + n2
-resta = n1 - n2
-multi = n1 * n2
-div = n1 / n2
+def resta(x, y) :
+    return x - y
 
-mensaje = f"""
-Para los numeros {n1} y {n2},
-el resultado de la suma es {suma},
-el resultado de la resta es {resta},
-el resultado de la multiplicacion es {multi}
-el resultado de la division es {div} 
-"""
-print(mensaje)
+def multiplicacion(x, y) :
+    return x * y
+
+def division(x, y) :
+    if y !=0 :
+        return x / y
+    else :
+        return "Error: division por cero"
+    
+# Menu de la calculadora
+
+print("Seleccione una operacion")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicacion")
+print("4. Division")
+
+# Solicitar al usuario que elija una operacion
+
+opcion = input("Ingrese el numero de la operacion deseada: ")
+
+# Solicitar al usuario q ingrese dos  numeros
+
+num1= float(input("Ingrese el primer numero: "))
+num2= float(input("Ingrese el segundo numero: "))
+    
+# Realizar la operacion segun la eleccion del usuario
+
+if opcion == "1" :
+    print(num1, "+", num2, "=" , suma(num1, num2))
+
+elif opcion == "2" :
+    print(num1, "-", num2, "=" , suma(num1, num2))
+
+elif opcion == "3" :
+    print(num1, "*", num2, "=" , suma(num1, num2))
+
+elif opcion == "4" :
+    print(num1, "/", num2, "=" , suma(num1, num2))
+    
+else:
+    print("Operacion no valida")
